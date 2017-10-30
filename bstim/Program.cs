cd11578ddac9b9828ba3e9440cf01f2c6c59afa8
xyx0826 <xyx0826@hotmail.com>
2017-10-30 11:39:26 -0400

    添加项目文件。

---
+++ bstim/Program.cs
@@ -0,0 +1,129 @@
+﻿/* Author: daemon1 (zenhax)
+ * Re-compiled by xyx0826
+ */
+
+using System;
+using System.IO;
+
+namespace bitsquid_timpani
+{
+    internal class Program
+    {
+        private static void WriteWavHeader(BinaryWriter bw, int channels, int bits, int samplerate, int bytes)
+        {
+            bw.Write("RIFF");
+            bw.Write(bytes + 36);
+            bw.Write("WAVEfmt ");
+            bw.Write(16);
+            bw.Write(1);
+            bw.Write((short)channels);
+            bw.Write(samplerate);
+            bw.Write(samplerate * channels * bits / 8);
+            bw.Write((short)(channels * bits / 8));
+            bw.Write((short)bits);
+            bw.Write("data");
+            bw.Write(bytes);
+        }
+
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Bitsquid Toolchain - bstim (bitsquid_timpany) by daemon1");
+            Console.WriteLine("Modified by xyx0826");
+            Console.WriteLine("Usage: bstim.exe [bank-name]");
+            if (args.Length == 0) // If no parameters are given
+            {
+                Console.WriteLine("ERROR: bank file not specified.");
+                Console.ReadKey();
+                return;
+            }
+            bool isStreamFound = false;
+            try
+            {
+                if (File.Exists(args[0] + ".stream")) isStreamFound = true;
+            }
+            catch
+            {
+                Console.WriteLine("INFO: stream file not found.");
+            }
+            FileStream bankFile = new FileStream(args[0], FileMode.Open);
+            BinaryReader binaryReader = new BinaryReader(bankFile);
+            FileStream streamFile = null;
+            int fileCounts = (int)binaryReader.ReadInt64();
+            int[] array = new int[fileCounts];
+            int[] array2 = new int[fileCounts];
+            ulong[] outFileName = new ulong[fileCounts];
+            int[] array4 = new int[fileCounts];
+            int[] array5 = new int[fileCounts];
+            if (isStreamFound) streamFile = new FileStream(args[0] + ".stream", FileMode.Open);
+            for (long num2 = 0L; num2 < fileCounts; num2 += 1L)
+            {
+                checked
+                {
+                    outFileName[(int)((IntPtr)num2)] = binaryReader.ReadUInt64();
+                    array[(int)((IntPtr)num2)] = binaryReader.ReadInt32();
+                    array2[(int)((IntPtr)num2)] = binaryReader.ReadInt32();
+                    if (isStreamFound)
+                    {
+                        array4[(int)((IntPtr)num2)] = binaryReader.ReadInt32();
+                        array5[(int)((IntPtr)num2)] = binaryReader.ReadInt32();
+                    }
+                }
+            }
+            long num3 = 0L;
+            while (num3 < fileCounts)
+            {
+                bankFile.Seek(array[(int)(checked(num3))], SeekOrigin.Begin);
+                int num4 = binaryReader.ReadInt32();
+                if (num4 != 68)
+                {
+                    Console.WriteLine("INFO: unknown sound format.");
+                }
+                int bytes = binaryReader.ReadInt32();
+                bankFile.Seek(40L, SeekOrigin.Current);
+                int num5 = binaryReader.ReadInt16();
+                FileStream fileStream3;
+                if (num5 == 1)
+                {
+                    int channels = binaryReader.ReadInt16();
+                    int samplerate = binaryReader.ReadInt32();
+                    binaryReader.ReadInt32();
+                    binaryReader.ReadInt16();
+                    int bits = binaryReader.ReadInt16();
+                    binaryReader.ReadInt32();
+                    fileStream3 = new FileStream(outFileName[(int)(checked(num3))].ToString("X16") + ".wav", FileMode.Create);
+                    BinaryWriter bw = new BinaryWriter(fileStream3);
+                    WriteWavHeader(bw, channels, bits, samplerate, bytes);
+                    goto IL_1D1;
+                }
+                if (num5 == 22127)
+                {
+                    fileStream3 = new FileStream(outFileName[(int)(checked(num3))].ToString("X16") + ".ogg", FileMode.Create);
+                    bankFile.Seek((array[(int)(checked(num3))] + 68), SeekOrigin.Begin);
+                    goto IL_1D1;
+                }
+                Console.WriteLine("INFO: unknown sound format.");
+                IL_259:
+                num3 += 1L;
+                continue;
+                IL_1D1:
+                byte[] buffer = new byte[array2[(int)(checked(num3))] - 68];
+                bankFile.Read(buffer, 0, array2[(int)(checked(num3))] - 68);
+                fileStream3.Write(buffer, 0, array2[(int)(checked(num3))] - 68);
+                checked
+                {
+                    if (isStreamFound && streamFile != null && array5[(int)((IntPtr)num3)] > 0)
+                    {
+                        streamFile.Seek(unchecked(array4[(int)(checked(num3))]), SeekOrigin.Begin);
+                        buffer = new byte[array5[(int)((IntPtr)num3)]];
+                        streamFile.Read(buffer, 0, array5[(int)((IntPtr)num3)]);
+                        fileStream3.Write(buffer, 0, array5[(int)((IntPtr)num3)]);
+                    }
+                    fileStream3.Close();
+                    goto IL_259;
+                }
+            }
+            Console.WriteLine("Extraction complete.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ bsunp/Program.cs
@@ -0,0 +1,173 @@
+﻿/* Author: daemon1 (zenhax)
+ * Re-compiled by xyx0826
+ */
+
+using System;
+using System.IO;
+using zlib;
+
+namespace bitsquid_unp
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Bitsquid Toolchain - bsunp (bitsquid_unp) by daemon1");
+            Console.WriteLine("Modified by xyx0826");
+            Console.WriteLine("Usage: bsunp.exe [package-name]");
+            if (args.Length == 0) // If no parameters are given
+            {
+                Console.WriteLine("ERROR: package file not specified.");
+                Console.ReadKey();
+                return;
+            }
+
+            // Read package file
+            FileStream packageFile = new FileStream(args[0], FileMode.Open);
+            BinaryReader binaryReader = new BinaryReader(packageFile);
+            MemoryStream memoryStream = new MemoryStream();
+            String path = Path.GetDirectoryName(
+                System.Reflection.Assembly.GetExecutingAssembly().Location);
+
+            // Check if stream file exists with package file
+            FileStream streamFile = null;
+            if (File.Exists(args[0] + ".stream"))
+            {
+                streamFile = new FileStream(args[0] + ".stream", FileMode.Open);
+            }
+            else Console.WriteLine("INFO: stream file not found.");
+            packageFile.Seek(12L, SeekOrigin.Begin);
+            byte[] buffer = new byte[66000];
+            while (packageFile.Position < packageFile.Length)
+            {
+                int num = binaryReader.ReadInt32();
+                packageFile.Read(buffer, 0, num);
+                if (num == 65536)
+                {
+                    memoryStream.Write(buffer, 0, num);
+                }
+                else
+                {
+                    ZOutputStream zOutputStream = new ZOutputStream(memoryStream);
+                    zOutputStream.Write(buffer, 0, num);
+                    zOutputStream.Flush();
+                }
+            }
+            binaryReader = new BinaryReader(memoryStream);
+            memoryStream.Seek(4L, SeekOrigin.Begin);
+
+            Directory.CreateDirectory(path + "\\extracted");
+
+            // Write data.bin
+            byte[] buffer2 = new byte[288];
+            binaryReader.Read(buffer2, 0, 288);
+            FileStream dataFile = new FileStream(path + "\\extracted\\data.bin", FileMode.Create);
+            dataFile.Write(buffer2, 0, 288);
+            dataFile.Close();
+            memoryStream.Seek(0L, SeekOrigin.Begin);
+
+            if (!File.Exists(path + "\\hashdict.txt"))
+            {
+                Console.WriteLine("ERROR: hashdict.txt not found.");
+                Console.ReadKey();
+                return;
+            }
+
+            // Read hash dictionary
+            StreamReader streamReader = new StreamReader(path + "\\hashdict.txt");
+            int hashCount = Convert.ToInt32(streamReader.ReadLine());
+            ulong[] hashes = new ulong[hashCount];
+            string[] types = new string[hashCount];
+            for (int i = 0; i < hashCount; i++)
+            {
+                string hashEntry = streamReader.ReadLine();
+                int tabPos = hashEntry.IndexOf('\t', 0);
+                hashes[i] = Convert.ToUInt64(hashEntry.Substring(0, tabPos), 16);
+                types[i] = hashEntry.Substring(tabPos + 1);
+            }
+            int num4 = binaryReader.ReadInt32();
+            memoryStream.Seek(256L, SeekOrigin.Current);
+
+            // Identify file hashes
+            for (int j = 0; j < num4; j++)
+            {
+                ulong currentHash = binaryReader.ReadUInt64();
+                binaryReader.ReadUInt64();
+                bool known = false;
+                for (int i = 0; i < hashCount; i++)
+                {
+                    if (currentHash == hashes[i])
+                    {
+                        Console.WriteLine("INFO: extracting file " + types[i]);
+                        known = true;
+                        break;
+                    }
+                }
+                if (!known)
+                {
+                    Console.WriteLine("INFO: file hash unknown " + currentHash);
+                }
+            }
+            for (int j = 0; j < num4; j++)
+            {
+                ulong num6 = binaryReader.ReadUInt64();
+                ulong num7 = binaryReader.ReadUInt64();
+                string outputType = num6.ToString("X16");
+                for (int i = 0; i < hashCount; i++)
+                {
+                    if (num6 == hashes[i])
+                    {
+                        outputType = types[i];
+                        break;
+                    }
+                }
+                string outputHash = num7.ToString("X16");
+                int num8 = binaryReader.ReadInt32();
+                binaryReader.ReadInt32();
+                int[] array3 = new int[num8];
+                int[] array4 = new int[num8];
+                int[] array5 = new int[num8];
+                for (int i = 0; i < num8; i++)
+                {
+                    array4[i] = binaryReader.ReadInt32();
+                    array3[i] = binaryReader.ReadInt32();
+                    array5[i] = binaryReader.ReadInt32();
+                }
+                // Write output file
+                for (int i = 0; i < num8; i++)
+                {
+                    string outputFileName;
+                    if (i == 0)
+                    {
+                        outputFileName = outputType.Substring(0, 3) + "_" + outputHash + "." + outputType;
+                    }
+                    else
+                    {
+                        outputFileName = string.Concat(new string[]
+                        {
+                            outputType.Substring(0, 3),
+                            "_",
+                            outputHash,
+                            "_",
+                            array4[i].ToString("X"),
+                            ".",
+                            outputType
+                        });
+                    }
+                    byte[] array6 = new byte[array3[i]];
+                    memoryStream.Read(array6, 0, array3[i]);
+                    File.WriteAllBytes(path + "\\extracted\\" + outputFileName, array6);
+
+                    if (streamFile != null && array5[i] > 0)
+                    {
+                        byte[] array7 = new byte[array5[i]];
+                        streamFile.Read(array7, 0, array5[i]);
+                        File.WriteAllBytes(path + "\\extracted\\" + outputFileName + ".stream", array7);
+                    }
+                }
+            }
+            Console.WriteLine("Extraction complete.");
+            Console.ReadKey();
+        }
+    }
+}
